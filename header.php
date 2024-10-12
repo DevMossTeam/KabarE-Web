608d09a7df969104366901d2e8981849255dd33e
Basisty034 <158120171+Basisty034@users.noreply.github.com>
2024-10-12 09:40:28 +0700

    header update

--- header.php
+++ header.php
@@ -5,7 +5,7 @@ if (session_status() === PHP_SESSION_NONE) {
 include 'connection/config.php';
 
 // Redirect ke index.php jika halaman ini diakses langsung dan bukan halaman yang diizinkan
-$allowed_pages = ['index.php', 'previewAuthor.php', 'publishAuthor.php', 'Main_author.php', 'reviewStat.php', 'mainEditor.php', 'kampus.php', 'prestasi.php', 'politik.php', 'kesehatan.php', 'olahraga.php', 'ekonomi.php', 'bisnis.php', 'ukm.php', 'berita_lainnya.php'];
+$allowed_pages = ['index.php', 'previewAuthor.php', 'publishAuthor.php', 'Main_author.php', 'reviewStat.php', 'mainEditor.php', 'kampus.php', 'search.php', 'prestasi.php', 'politik.php', 'kesehatan.php', 'olahraga.php', 'ekonomi.php', 'bisnis.php', 'ukm.php', 'berita_lainnya.php'];
 $current_page = basename($_SERVER['PHP_SELF']);
 
 if (!in_array($current_page, $allowed_pages)) {
